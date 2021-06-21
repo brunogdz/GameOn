884a320d2a902cb511b6fc2d61c7ae7301a72749
Bruno Gomes <brunog.vix@gmail.com>
2021-06-21 15:11:43 -0300

    adicionado as imagens e criado a página de Login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App(){
   return(
-    <View>
-      <Text></Text>
-    </View>
+   <SignIn />
   );
 }
