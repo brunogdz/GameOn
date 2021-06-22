fa340a29e09d5a27f1d2ebd072264d18dbf99812
Bruno Gomes <brunog.vix@gmail.com>
2021-06-22 00:00:37 -0300

    importando as fonts e caso não tenha carregado fica no loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {useFonts} from 'expo-font';
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App(){
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
   return(
    <SignIn />
   );
