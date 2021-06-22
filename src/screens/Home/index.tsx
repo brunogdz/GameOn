d7c3f082cb55746457d85acd213c997f17b830d5
Bruno Gomes <brunog.vix@gmail.com>
2021-06-22 16:59:21 -0300

    ao fazer o header inserindo lembrando que nos novos modelos de iphone poderia dar error

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import { styles } from './styles';
+
+export function Home(){
+    return (
+        <View>
+            <View style={styles.header}>
+
+            </View>
+        </View>
+    );
+}
