a6e6efa4a3c9d5afe4cb2086267d8ae602794e7e
Bruno Gomes <brunog.vix@gmail.com>
2021-06-22 01:04:45 -0300

    feito o gradiente

--- App.tsx
+++ App.tsx
@@ -3,8 +3,11 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
+import {StatusBar} from 'react-native';
+
 import { SignIn } from './src/screens/SignIn';
-import {StatusBar} from 'react-native'
+import {Background} from './src/components/Background'
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,14 +21,14 @@ export default function App() {
     return <AppLoading />
   }
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   );
 
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -7,7 +7,6 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.colors.secondary100
   },
   image: {
     width: '100%',
