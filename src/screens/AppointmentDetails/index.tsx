860f25a708fbffa72699706b85ca075765963e4a
Bruno Gomes <brunog.vix@gmail.com>
2021-06-25 20:50:17 -0300

    criado o header

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import { Background } from '../../components/Background';
+
+
+export function AppointmentDetails() {
+    return (
+        <Background>
+            
+        </Background>
+    )
+}
